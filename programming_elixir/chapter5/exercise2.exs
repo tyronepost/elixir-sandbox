80e19d6e015c7a9beb3fc5df1396857c0f3d163d
tyronepost <tyrone.post.a@gmail.com>
2016-10-21 21:00:06 -0400

    adding exercise 2 to exercise 2 file (fizzbuzz with rem function)

--- programming_elixir/chapter5/exercise2.exs
+++ programming_elixir/chapter5/exercise2.exs
@@ -5,9 +5,21 @@ fizzbuzz = fn
   (_, _, a) -> a
 end
 
+fizz_buzz_rem = fn
+  n -> fizzbuzz.(rem(n,3), rem(n,5), n)
+end
+
 IO.puts fizzbuzz.(0, 0, 1)
 IO.puts fizzbuzz.(0, 1, 1)
 IO.puts fizzbuzz.(1, 0, 1)
 IO.puts fizzbuzz.(1, 1, 1)
+
+IO.puts fizz_buzz_rem.(10)
+IO.puts fizz_buzz_rem.(11)
+IO.puts fizz_buzz_rem.(12)
+IO.puts fizz_buzz_rem.(13)
+IO.puts fizz_buzz_rem.(14)
+IO.puts fizz_buzz_rem.(15)
+IO.puts fizz_buzz_rem.(16)
 
 
