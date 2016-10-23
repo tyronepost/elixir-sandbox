a572354303b2dd34dc97e622ff27a712eeab2d94
tyronepost <tyrone.post.a@gmail.com>
2016-10-23 08:05:30 -0400

    refactoring guess.exs

--- programming_elixir/chapter6/guess.exs
+++ programming_elixir/chapter6/guess.exs
@@ -1,19 +1,17 @@
 defmodule Chop do
-  def guess(num, range) do
-    current_guess = div(range.last - range.first, 2) + range.first
+  def guess(num, low..high) do
+    current_guess = div(low + high, 2) 
     IO.puts "Is it #{current_guess}"
-    is_it(num, current_guess, range)
+    is_it(num, current_guess, low..high)
   end
   
   def is_it(num, current_guess, _) when num === current_guess do
     current_guess
   end
-
-  def is_it(num, current_guess, range) when num > current_guess do
-    guess(num, current_guess..range.last)
+  def is_it(num, current_guess, _..high) when num > current_guess do
+    guess(num, current_guess+1..high)
   end
-
-  def is_it(num, current_guess, range) when num < current_guess do
-    guess(num, range.first..current_guess)
+  def is_it(num, current_guess, low.._) when num < current_guess do
+    guess(num, low..current_guess-1)
   end
 end
