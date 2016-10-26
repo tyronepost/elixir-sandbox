1883201518b098f5ed7a3980898058cfeeb8ed3e
tyronepost <tyrone.post.a@gmail.com>
2016-10-25 20:28:38 -0400

    adding map function to mylist module

--- programming_elixir/chapter7/mylist.exs
+++ programming_elixir/chapter7/mylist.exs
@@ -1,4 +1,7 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail) 
+
+   def map([], _func), do: [] 
+   def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
 end
