6f51250ac73e2d1e91afc4699d495dd4a73f9c4b
tyronepost <tyrone.post.a@gmail.com>
2016-10-25 20:57:08 -0400

    adding sum to mylist.exs

--- programming_elixir/chapter7/mylist.exs
+++ programming_elixir/chapter7/mylist.exs
@@ -2,6 +2,12 @@ defmodule MyList do
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail) 
 
-   def map([], _func), do: [] 
-   def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
+  def map([], _func), do: [] 
+  def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
+
+  def sum(list), do: _sum(list, 0)
+
+  # private methods
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, head + total)
 end
