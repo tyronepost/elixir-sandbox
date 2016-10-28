ae2c6e278fa187e713e1995be085fcd0a6f28e06
tyronepost <tyrone.post.a@gmail.com>
2016-10-27 21:18:51 -0400

    cleaning up code

--- programming_elixir/chapter7/mylist.exs
+++ programming_elixir/chapter7/mylist.exs
@@ -1,18 +1,16 @@
 defmodule MyList do
 
-  def max(list) do
-    _max(list, 0)
-  end
+  def max(list), do: _max(list, 0)
+  # private max functions 
+  defp _max([], max), do: max 
   defp _max([head | tail], max) when head < max do
     _max(tail, max)
   end
   defp _max([head | tail], max) when head > max do
     _max(tail, head)
   end
-  defp _max([], max) do
-    max
-  end
   
+  # reduce function
   def reduce([], value, _) do
     value
   end
@@ -20,22 +18,26 @@ defmodule MyList do
     reduce(tail, func.(head, value), func)
   end
 
+  # map functions
+  def map([], _func), do: [] 
+  def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
+
+  # summing map results
   def mapsum(list, func) do
     sum(map(list, func))
   end
   
+  # length functions
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail) 
-
-  def map([], _func), do: [] 
-  def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
-
+    
+  # sum functions
   def sum(list), do: _sum(list, 0)
-
   # private methods
   defp _sum([], total), do: total
   defp _sum([head | tail], total), do: _sum(tail, head + total)
 
+  # sum without partial total
   def sum2([]), do: 0
   def sum2([head | tail]), do: head + sum2(tail)
 end
